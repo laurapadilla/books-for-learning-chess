51d7c53243cdaedd58ed3512bb377ad57076ca49
laurapadilla <hi@laurapadilla.xyz>
2024-06-17 14:16:26 -0400

    fix some terminal warnings

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,25 @@
 import { Book } from '@/components';
 import { getBooks } from '@/sanity/cms';
+import Link from 'next/link';
 import * as styles from '../styles/globals.css';
 
 export default async function Home() {
   const books = await getBooks();
 
   return (
-    <div className={styles.booksContainer}>
-      {books.map((book) => {
-        return <Book book={book} key={book._id} />;
-      })}
-    </div>
+    <main>
+      <header>
+        <h1>Books for learning chess</h1>
+        <nav>
+          <Link href="https://www.chess.com/learn">learn chess</Link>
+          <Link href="https://www.chess.com">play chess</Link>
+        </nav>
+      </header>
+      <div className={styles.booksContainer}>
+        {books.map((book) => {
+          return <Book book={book} key={book._id} />;
+        })}
+      </div>
+    </main>
   );
 }
--- sanity.config.ts
+++ sanity.config.ts
@@ -2,21 +2,17 @@
 /**
  * This configuration is used to for the Sanity Studio that’s mounted on the `/app/studio/[[...tool]]/page.tsx` route
  */
+import { colorInput } from '@sanity/color-input';
 import { visionTool } from '@sanity/vision';
 import { defineConfig } from 'sanity';
 import { structureTool } from 'sanity/structure';
-
 // Go to https://www.sanity.io/docs/api-versioning to learn how API versioning works
-import { apiVersion, dataset, projectId } from './sanity/env';
+import { apiVersion, dataset, projectId, useCdn } from './sanity/env';
 import { schema } from './sanity/schema';
-import { colorInput } from '@sanity/color-input';
 
 export default defineConfig({
   basePath: '/studio',
-  projectId,
   dataset,
-  // Add and edit the content schema in the './sanity/schema' folder
-  schema,
   plugins: [
     colorInput(),
     structureTool(),
@@ -24,4 +20,8 @@ export default defineConfig({
     // https://www.sanity.io/docs/the-vision-plugin
     visionTool({ defaultApiVersion: apiVersion }),
   ],
+  projectId,
+  // Add and edit the content schema in the './sanity/schema' folder
+  schema,
+  useCdn,
 });
