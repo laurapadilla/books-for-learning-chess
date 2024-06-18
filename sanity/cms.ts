375b030236b64d6c777bb99d4295404eb6975737
laurapadilla <hi@laurapadilla.xyz>
2024-06-18 10:43:42 -0400

    update configs

--- sanity/cms.ts
+++ sanity/cms.ts
@@ -3,9 +3,9 @@ import { createClient, groq } from 'next-sanity';
 
 export async function getBooks(): Promise<Book[]> {
   const sanityClient = createClient({
-    projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
-    dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
     apiVersion: '2024-06-14',
+    dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
+    projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
   });
 
   return sanityClient.fetch(groq`*[_type == "book"]{
