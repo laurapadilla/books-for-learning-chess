7cdfcb448a44cfaef9d4e8d59e2b972b6a8e61b4
laurapadilla <hi@laurapadilla.xyz>
2024-06-26 12:55:16 -0400

    fix it

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,7 +15,7 @@ export const metadata: Metadata = {
   description:
     'Best books for learning chess and improving your game no matter your rating. These books are idea for beginners, intermediate, and advanced players as well as club players and professionals.',
   openGraph: {
-    images: ['/public/openGraph.png'],
+    images: ['./opengraph-image.png'],
   },
   title: 'Books for learning chess',
 };
