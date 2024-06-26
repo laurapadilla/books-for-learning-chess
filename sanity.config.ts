6133e42bb7d1cee8ee04d40d393806961955a13d
laurapadilla <hi@laurapadilla.xyz>
2024-06-26 09:43:38 -0400

    remove comments

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,12 +1,9 @@
 'use client';
-/**
- * This configuration is used to for the Sanity Studio that’s mounted on the `/app/studio/[[...tool]]/page.tsx` route
- */
+
 import { colorInput } from '@sanity/color-input';
 import { visionTool } from '@sanity/vision';
 import { defineConfig } from 'sanity';
 import { structureTool } from 'sanity/structure';
-// Go to https://www.sanity.io/docs/api-versioning to learn how API versioning works
 import { apiVersion, dataset, projectId, useCdn } from './sanity/env';
 import { schema } from './sanity/schema';
 
@@ -16,12 +13,9 @@ export default defineConfig({
   plugins: [
     colorInput(),
     structureTool(),
-    // Vision is a tool that lets you query your content with GROQ in the studio
-    // https://www.sanity.io/docs/the-vision-plugin
     visionTool({ defaultApiVersion: apiVersion }),
   ],
   projectId,
-  // Add and edit the content schema in the './sanity/schema' folder
   schema,
   useCdn,
 });
